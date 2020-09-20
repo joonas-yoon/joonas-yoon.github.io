2686aaee3db68fb3cdf5c45807a7bb372ae04f49
Joona Yoon <joonas.yoon@gmail.com>
2020-09-20 18:01:28 +0900

    Convert tabs to 2 spaces

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,173 +1,173 @@
 /*
-	Stellar by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+  Stellar by HTML5 UP
+  html5up.net | @ajlkn
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-		xxsmall: '(max-width: 360px)'
-	});
+  skel.breakpoints({
+    xlarge: '(max-width: 1680px)',
+    large: '(max-width: 1280px)',
+    medium: '(max-width: 980px)',
+    small: '(max-width: 736px)',
+    xsmall: '(max-width: 480px)',
+    xxsmall: '(max-width: 360px)'
+  });
 
-	$(function() {
+  $(function() {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$main = $('#main');
+    var	$window = $(window),
+      $body = $('body'),
+      $main = $('#main');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+    $window.on('load', function() {
+      window.setTimeout(function() {
+        $body.removeClass('is-loading');
+      }, 100);
+    });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+    // Fix: Placeholder polyfill.
+    $('form').placeholder();
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+    // Prioritize "important" elements on medium.
+    skel.on('+medium -medium', function() {
+      $.prioritize(
+        '.important\\28 medium\\29',
+        skel.breakpoint('medium').active
+      );
+    });
 
-		// Nav.
-			var $nav = $('#nav');
+    // Nav.
+    var $nav = $('#nav');
 
-			if ($nav.length > 0) {
+    if ($nav.length > 0) {
 
-				// Shrink effect.
-					$main
-						.scrollex({
-							mode: 'top',
-							enter: function() {
-								$nav.addClass('alt');
-							},
-							leave: function() {
-								$nav.removeClass('alt');
-							},
-						});
+      // Shrink effect.
+      $main
+        .scrollex({
+          mode: 'top',
+          enter: function() {
+            $nav.addClass('alt');
+          },
+          leave: function() {
+            $nav.removeClass('alt');
+          },
+        });
 
-				// Links.
-					var $nav_a = $nav.find('a');
+      // Links.
+      var $nav_a = $nav.find('a');
 
-					$nav_a
-						.scrolly({
-							speed: 1000,
-							offset: function() { return $nav.height(); }
-						})
-						.on('click', function() {
+      $nav_a
+        .scrolly({
+          speed: 1000,
+          offset: function() { return $nav.height(); }
+        })
+        .on('click', function() {
 
-							var $this = $(this);
+          var $this = $(this);
 
-							// External link? Bail.
-								if ($this.attr('href').charAt(0) != '#')
-									return;
+          // External link? Bail.
+          if ($this.attr('href').charAt(0) != '#')
+            return;
 
-							// Deactivate all links.
-								$nav_a
-									.removeClass('active')
-									.removeClass('active-locked');
+          // Deactivate all links.
+          $nav_a
+            .removeClass('active')
+            .removeClass('active-locked');
 
-							// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-								$this
-									.addClass('active')
-									.addClass('active-locked');
+          // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+          $this
+            .addClass('active')
+            .addClass('active-locked');
 
-						})
-						.each(function() {
+        })
+        .each(function() {
 
-							var	$this = $(this),
-								id = $this.attr('href'),
-								$section = $(id);
+          var	$this = $(this),
+            id = $this.attr('href'),
+            $section = $(id);
 
-							// No section for this link? Bail.
-								if ($section.length < 1)
-									return;
+          // No section for this link? Bail.
+          if ($section.length < 1)
+            return;
 
-							// Scrollex.
-								$section.scrollex({
-									mode: 'middle',
-									initialize: function() {
+          // Scrollex.
+          $section.scrollex({
+            mode: 'middle',
+            initialize: function() {
 
-										// Deactivate section.
-											if (skel.canUse('transition'))
-												$section.addClass('inactive');
+              // Deactivate section.
+              if (skel.canUse('transition'))
+                $section.addClass('inactive');
 
-									},
-									enter: function() {
+            },
+            enter: function() {
 
-										// Activate section.
-											$section.removeClass('inactive');
+              // Activate section.
+              $section.removeClass('inactive');
 
-										// No locked links? Deactivate all links and activate this section's one.
-											if ($nav_a.filter('.active-locked').length == 0) {
+              // No locked links? Deactivate all links and activate this section's one.
+              if ($nav_a.filter('.active-locked').length == 0) {
 
-												$nav_a.removeClass('active');
-												$this.addClass('active');
+                $nav_a.removeClass('active');
+                $this.addClass('active');
 
-											}
+              }
 
-										// Otherwise, if this section's link is the one that's locked, unlock it.
-											else if ($this.hasClass('active-locked'))
-												$this.removeClass('active-locked');
+              // Otherwise, if this section's link is the one that's locked, unlock it.
+              else if ($this.hasClass('active-locked'))
+                $this.removeClass('active-locked');
 
-									}
-								});
+            }
+          });
 
-						});
+        });
 
-			}
+    }
 
-		// Scrolly.
-			$('.scrolly').scrolly({
-				speed: 750
-			});
-		
-		function isIE() {
-			ua = navigator.userAgent;
-			/* MSIE used to detect old browsers and Trident used to newer ones*/
-			var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
-			return is_ie; 
-		}
-			
-		// animation video
-		var video = document.getElementById('video-anim');
-		if(!isIE() && !!document.createElement('video').canPlayType) {
-			var video = video.querySelector('video');
-			video.style.display = 'block';
-			video.autoplay = true;
-			video.muted = true;
-			video.controls = false;
-			video.load();
-			var playPromise = video.play();
-			// In browsers that don’t yet support this functionality,
-			// playPromise won’t be defined.
-			if (playPromise !== undefined) {
-				playPromise.then(function() {
-					// Automatic playback started!
-				}).catch(function(error) {
-					// Automatic playback failed.
-					// Show a UI element to let the user manually start playback.
-					video.querySelector('img').style.display = 'block';
-					console.error(error);
-				});
-			}
-		}
-		else {
-			video.querySelector('img').style.display = 'block';
-		}
+    // Scrolly.
+    $('.scrolly').scrolly({
+      speed: 750
+    });
+    
+    function isIE() {
+      ua = navigator.userAgent;
+      /* MSIE used to detect old browsers and Trident used to newer ones*/
+      var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+      return is_ie; 
+    }
+      
+    // animation video
+    var video = document.getElementById('video-anim');
+    if(!isIE() && !!document.createElement('video').canPlayType) {
+      var video = video.querySelector('video');
+      video.style.display = 'block';
+      video.autoplay = true;
+      video.muted = true;
+      video.controls = false;
+      video.load();
+      var playPromise = video.play();
+      // In browsers that don’t yet support this functionality,
+      // playPromise won’t be defined.
+      if (playPromise !== undefined) {
+        playPromise.then(function() {
+          // Automatic playback started!
+        }).catch(function(error) {
+          // Automatic playback failed.
+          // Show a UI element to let the user manually start playback.
+          video.querySelector('img').style.display = 'block';
+          console.error(error);
+        });
+      }
+    }
+    else {
+      video.querySelector('img').style.display = 'block';
+    }
 
-	});
+  });
 
 })(jQuery);
