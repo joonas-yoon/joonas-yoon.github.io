4c2a6dcef5789e0e9782874c8f6a509b01e37bfa
Joona Yoon <joonas.yoon@gmail.com>
2021-01-20 14:42:42 +0900

    cleanup: remove deprecated and fix bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-  Stellar by HTML5 UP
-  html5up.net | @ajlkn
-  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function ($) {
   $(function () {
     const $window = $(window);
@@ -17,47 +11,6 @@
       window.setTimeout(function () {
         $body.removeClass('is-loading');
       }, 100);
-    });
-
-    // Nav.
-    const $nav = $('#nav');
-
-    if ($nav.length > 0) {
-      // Links.
-      const $navA = $nav.find('a');
-
-      $navA
-        .scrolly({
-          speed: 1000,
-          offset: function () {
-            return $nav.height();
-          },
-        })
-        .on('click', function () {
-          const $this = $(this);
-
-          // External link? Bail.
-          if ($this.attr('href').charAt(0) != '#') return;
-
-          // Deactivate all links.
-          $navA.removeClass('active').removeClass('active-locked');
-
-          // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-          $this.addClass('active').addClass('active-locked');
-        })
-        .each(function () {
-          const $this = $(this);
-          const id = $this.attr('href');
-          const $section = $(id);
-
-          // No section for this link? Bail.
-          if ($section.length < 1) return;
-        });
-    }
-
-    // Scrolly.
-    $('.scrolly').scrolly({
-      speed: 750,
     });
 
     // not support IE, safari yet.
