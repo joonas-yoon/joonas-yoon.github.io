03feb58edb22f6344da17cb76ceb0d4f088d90ac
Joona Yoon <joonas.yoon@gmail.com>
2021-05-31 21:20:01 +0900

    feat: add geometric animate dots on background

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,5 +171,130 @@
     $('.modal-close-button').on('click', function (e) {
       $modal.fadeOut();
     });
+
+    function Dot(x, y) {
+      const self = this;
+
+      this.speed = 0.3;
+      this.dx = Math.random() * (Math.random() < 0.5 ? -1 : 1) * this.speed;
+      this.dy = Math.random() * (Math.random() < 0.5 ? -1 : 1) * this.speed;
+      this.dz = Math.random() * (Math.random() < 0.5 ? -1 : 1) * 0.1;
+      this.x = x;
+      this.y = y;
+      this.z = Math.random();
+      this.width = 0;
+      this.height = 0;
+
+      const setBounds = function (width, height) {
+        self.width = width;
+        self.height = height;
+      };
+
+      const drawLine = function (context, p1, p2, color) {
+        context.beginPath();
+        context.moveTo(p1.getX(), p1.getY());
+        context.lineTo(p2.getX(), p2.getY());
+        context.closePath();
+        context.lineWidth = 1;
+        context.strokeStyle = color || '#000';
+        context.stroke();
+      };
+
+      const drawCircle = function (context, x1, y1, radius, color) {
+        context.beginPath();
+        context.arc(x1, y1, radius, 0, 2 * Math.PI);
+        context.closePath();
+        context.fillStyle = color || '#000';
+        context.fill();
+      };
+
+      const draw = function (context) {
+        const opa = (1.5 - self.z) / 200 + 0.5;
+        drawCircle(
+          context,
+          self.x,
+          self.y,
+          Math.max(1, 2 - self.z),
+          'rgba(255, 255, 255, ' + opa + ')'
+        );
+      };
+
+      const move = function () {
+        const nx = self.x + self.dx;
+        const ny = self.y + self.dy;
+        const nz = self.z + self.dz;
+        if (nx <= 0 || nx >= self.width) self.dx *= -1;
+        if (ny <= 0 || ny >= self.height) self.dy *= -1;
+        if (ny <= 0 || ny >= 1.5) self.dz *= -1;
+        self.x += self.dx;
+        self.y += self.dy;
+        self.z += self.dz;
+      };
+
+      const distance = function (p) {
+        const dx = self.x - p.getX();
+        const dy = self.y - p.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+      };
+
+      const getX = function () {
+        return self.x;
+      };
+      const getY = function () {
+        return self.y;
+      };
+
+      return {
+        getX: getX,
+        getY: getY,
+        setBounds: setBounds,
+        drawLine: drawLine,
+        drawCircle: drawCircle,
+        draw: draw,
+        move: move,
+        getDistance: distance,
+      };
+    }
+
+    const canvas = document.getElementById('canvas-bg');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const dots = [];
+    for (let i = 0; i < 50 + canvas.width / 20; ++i) {
+      dots.push(
+        new Dot(Math.random() * canvas.width, Math.random() * canvas.height)
+      );
+    }
+
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, true);
+    resizeCanvas();
+
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      // recalc bounds
+      dots.forEach((d) => {
+        d.setBounds(canvas.width, canvas.height);
+      });
+    }
+
+    function drawDots() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      dots.forEach((d, i) => {
+        d.draw(ctx);
+        for (let j = i + 1; j < dots.length; ++j) {
+          const dist = d.getDistance(dots[j]);
+          if (dist < 150) {
+            const opa = Math.max(0, 0.8 - dist / 150);
+            d.drawLine(ctx, d, dots[j], 'rgba(255, 255, 255, ' + opa + ')');
+          }
+        }
+        d.move();
+      });
+      window.requestAnimationFrame(drawDots);
+    }
+    window.requestAnimationFrame(drawDots);
   });
 })(jQuery);
